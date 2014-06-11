919afa56da98a77f3229f02da7fdecadf1ea96c4
Doug Anderson <douga@google.com>
2014-06-11 16:42:02 -0700

    Hello World Minimal Endpoints Backend API.

---
+++ src/main/java/com/example/helloworld/YourFirstAPI.java
@@ -0,0 +1,24 @@
+//[START all]
+package com.example.helloworld;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import javax.inject.Named;
+
+/** An endpoint class we are exposing */
+@Api(name = "myApi", version = "v1", namespace = @ApiNamespace(ownerDomain = "helloworld.example.com", ownerName = "helloworld.example.com", packagePath=""))
+public class YourFirstAPI {
+
+    /** A simple endpoint method that takes a name and says Hi back */
+    @ApiMethod(name = "sayHi")
+    public MyBean sayHi(@Named("name") String name) {
+        MyBean response = new MyBean();
+        response.setData("Hi, " + name);
+
+        return response;
+    }
+
+}
+//[END all]
